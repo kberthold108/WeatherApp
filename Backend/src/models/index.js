cba7ed33cfff6419b289f408c60d294471084adf
Kenneth <kenneth.berthold@quajoo.com>
2021-11-12 13:08:04 +0100

    Added Docker Container

--- Backend/src/models/index.js
+++ Backend/src/models/index.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 import Weather from "./weather.js";
 
 const connectDb = () => {
-    return mongoose.connect("mongodb://127.0.0.1:27017/weather-app");
+    return mongoose.connect("mongodb://database:27017/weather-app");
 }
 export { connectDb, Weather };
